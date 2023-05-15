8cf5d4914ce0017ebd73b37e2886c51609de2495
coffey-dev <scoffey@abc.gob.ar>
2023-05-15 10:28:07 -0300

    navbar styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,12 @@
-import './App.css';
-import reactLogo from "./assets/react-logo.png";
-import Navbar from "./components/navbar";
-import Main from "./components/main";
+import React from "react";
+import Navbar from "./components/Navbar";
+import Main from "./components/Main";
 
-
-
-function App() {
-
-
-  return (
-    <div>
-  <Navbar />
-  <Main />
-  </div>
-  )
+export default function App() {
+    return (
+        <div className="container">
+            <Navbar />
+            <Main />
+        </div>
+    )
 }
-
-export default App
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,5 +1,12 @@
-export default function Main() {
+import React from "react";
+
+
+export default function Navbar() {
     return (
-        <h1>Here goes the navbar component</h1>
+        <nav>
+            <img src="../src/images/react.png" className="nav--icon" />
+            <h3 className="nav--logo_text">ReactFacts</h3>
+            <h4 className="nav--title">React Course - Project 1</h4>
+        </nav>
     )
 }
